37287b284c67ed14df89930a36002a304221519f
VascoVerissimo <vasco.verissimo@uni.lu>
2019-06-18 10:48:50 +0200

    functions updated

--- test/io.jl
+++ test/io.jl
@@ -54,7 +54,7 @@ print(panel.fcs_colname)
 
 lineage_markers, functional_markers = getMarkers(panel)
 
-fcs_raw = readflowset(md.file_name)
+fcs_raw = readFlowset(md.file_name)
 cleannames!(fcs_raw)
 
 # subset the data
